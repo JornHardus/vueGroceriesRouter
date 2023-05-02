86084a3c4c38fd86998063dbe5414955e0afb330
JornHardus <113098382+JornHardus@users.noreply.github.com>
2023-05-02 11:10:11 +0200

    vueStore

---
+++ src/pages/groceries/Create.vue
@@ -0,0 +1,21 @@
+<script setup>
+import GroceryForm from "/src/components/GroceryForm.vue";
+import { addGrocery } from "/src/store/Groceries.js";
+
+// 1. import groceryform
+// 2. maak hier een leeg grocery template
+
+const newGrocery = {
+  Name: "",
+  Price: "",
+  Quantity: "",
+};
+
+const add = (groc) => {
+  addGrocery(groc);
+};
+</script>
+
+<template>
+  <GroceryForm :grocery="newGrocery" @update-product="add" />
+</template>
